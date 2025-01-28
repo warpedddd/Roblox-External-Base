05c49c35f6d738d914d7ca24a4e72b8cdd562c80
kai <110729110+warpedddd@users.noreply.github.com>
2025-01-28 12:25:31 +0300

    Update sdk.cpp

--- Roblox External Base/Roblox External Base/backend/core/roblox-development-kit/sdk.cpp
+++ Roblox External Base/Roblox External Base/backend/core/roblox-development-kit/sdk.cpp
@@ -13,14 +13,10 @@ Matrix4 Visualengine::GetViewMatrix() {
     return pMem->Rpm<Matrix4>(this->Address + VIEW_MATRIX);
 }
 
-Matrix4 Visualengine::SetViewMatrix(Matrix4 Point) {
-    return pMem->Wpm<Matrix4>(this->Address + VIEW_MATRIX, Point);
-}
-
 // w2s type shit
-Vectors::Vector2 w2s(Vectors::Vector3 RelativeLocation)
+Vectors::Vector2 w2s(Vectors::Vector3 RelativeLocation) // you dont need to use visual engine, you could also use camera
 {
-    Matrix4 ViewProjMatrix = Globals::Roblox::VisualEngine.GetViewMatrix();
+    Matrix4 ViewProjMatrix = Globals::Roblox::VisualEngine.GetViewMatrix(); 
     Vectors::Vector2 ViewPortSize = Globals::Roblox::VisualEngine.GetViewPortSize();
 
     Vectors::Vector4 transformed(
@@ -43,7 +39,7 @@ Vectors::Vector2 w2s(Vectors::Vector3 RelativeLocation)
 }
 
 // GameInstance Class
-std::string GameInstance::ReadString(uint64_t Address)
+std::string GameInstance::ReadString(uint64_t Address) // should be in driver class ngl
 {
     uint64_t StringCount = pMem->Rpm<uint64_t>(Address + STRING_LENGTH);
 
@@ -57,9 +53,7 @@ std::string GameInstance::ReadString(uint64_t Address)
     buffer.resize(StringCount);
 
     for (size_t i = 0; i < StringCount; ++i) 
-    {
         buffer[i] = pMem->Rpm<char>(Address + i);  
-    }
 
     return buffer;
 }
